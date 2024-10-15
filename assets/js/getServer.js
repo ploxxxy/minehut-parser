da12c2c332e72f688407e13921a829125cf1608c
ploxxy <71073152+ploxxxy@users.noreply.github.com>
2024-10-15 11:56:36 +0300

    updated color support and misc fixes

--- assets/js/getServer.js
+++ assets/js/getServer.js
@@ -1,51 +1,76 @@
 new gridjs.Grid({
-    width: '1460px',
-    columns: [{
-        name: 'Name',
-        data: row => row.name,
-        formatter: cell => { document.title = `${cell} - Minehut server lookup`; return cell },
-    },{
-        name: 'Categories',
-        data: row => row.categories.join(' ') || '-'
-    },{
-        name: 'Raw Motd',
-        data: row => row.motd,
-        width: '20%'
-    },{
-        name: 'Created',
-        data: row => row.creation,
-        formatter: (cell) => new Date(cell).toLocaleString()
-    },{
-        name: 'Online',
-        data: row => row.online ? `Yes (${row.playerCount} players)` : new Date(row.last_online).toLocaleDateString()
-    },{
-        name: 'Server Type',
-        data: row => row.server_version_type
-    },{
-        name: 'Active Plan',
-        data: row => `${row.activeServerPlan} (${row.rawPlan})` 
-    },{
-        name: 'Connected Servers',
-        data: row => row.connectedServers,
-        formatter: (cell) => gridjs.html(cell.join(' ').replace(/([^\s]+)/g, '<a href="/server.html?q=$1" target="_blank">$1</a>') || '-')
-    },{
-        name: 'Proxy',
-        data: row => row.proxy ? 'Yes' : 'No'
-    },{
-        name: 'Credits per day',
-        data: row => row.credits_per_day.toFixed()
-    }],
-
-    server: {
-        url: 'https://api.minehut.com/server/' + window.location.search.substring(3),
-        then: data => [data.server]
+  width: '1460px',
+  columns: [
+    {
+      name: 'Name',
+      data: (row) => row.name,
+      formatter: (cell) => {
+        document.title = `${cell} - Minehut server lookup`
+        return cell
+      },
     },
-    
-    style: {
-        td: {
-            'padding': '6px 12px',
-            'color': '#ccc'
-        }
-    }
+    {
+      name: 'Categories',
+      data: (row) => row.categories.join(' ') || '-',
+    },
+    {
+      name: 'Raw Motd',
+      data: (row) => row.motd,
+      width: '20%',
+    },
+    {
+      name: 'Created',
+      data: (row) => row.creation,
+      formatter: (cell) => new Date(cell).toLocaleString(),
+    },
+    {
+      name: 'Online',
+      data: (row) =>
+        row.online
+          ? `Yes (${row.playerCount} players)`
+          : new Date(row.last_online).toLocaleDateString(),
+    },
+    {
+      name: 'Server Type',
+      data: (row) => row.server_version_type,
+    },
+    {
+      name: 'Active Plan',
+      data: (row) => `${row.activeServerPlan} (${row.rawPlan})`,
+    },
+    {
+      name: 'Connected Servers',
+      data: (row) => row.connectedServers,
+      formatter: (cell) =>
+        gridjs.html(
+          cell
+            .join(' ')
+            .replace(
+              /([^\s]+)/g,
+              '<a href="/server.html?q=$1" target="_blank">$1</a>'
+            ) || '-'
+        ),
+    },
+    {
+      name: 'Proxy',
+      data: (row) => (row.proxy ? 'Yes' : 'No'),
+    },
+    {
+      name: 'Credits per day',
+      data: (row) => row.credits_per_day.toFixed(),
+    },
+  ],
 
+  server: {
+    url:
+      'https://api.minehut.com/server/' + window.location.search.substring(3),
+    then: (data) => [data.server],
+  },
+
+  style: {
+    td: {
+      padding: '6px 12px',
+      color: '#ccc',
+    },
+  },
 }).render(document.getElementById('table'))
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,102 +1,144 @@
 new gridjs.Grid({
-    fixedHeader: true,
-    width: '1460px',
-    height: '700px',
-    search: true,
-    sort: true,
-    pagination: {
-        enabled: true,
-        limit: 10,
-        buttonsCount: 1,
+  fixedHeader: true,
+  width: '1460px',
+  height: '700px',
+  search: true,
+  sort: true,
+  pagination: {
+    enabled: true,
+    limit: 10,
+    buttonsCount: 1,
+  },
+  resizable: true,
+  columns: [
+    {
+      name: 'ID',
+      data: (row) => row[0].staticInfo._id,
+      formatter: (cell) =>
+        gridjs.html(
+          `<a href="./server.html?q=${cell}" target="_blank">${cell}</a>`
+        ),
     },
-    columns: 
-    [{
-        name: 'ID',
-        data: row => row[0].staticInfo._id,
-        formatter: (cell) => gridjs.html(`<a href="./server.html?q=${cell}" target="_blank">${cell}</a>`),
-    },{
-        name: 'Name',
-        data: row => row[0].name,
-    },{
-        name: 'MOTD',
-        data: row => row[0].motd,
-        formatter: (cell) => cell.replace(/&.{1}/g, ''),
-        sort: { enabled: false },
-    },{
-        name: 'Players',
-        width: '12%',
-        data: row => `${row[0].playerData.playerCount}/${row[0].staticInfo.planMaxPlayers}`,
-        sort: {
-            compare: (a, b) => {
-                a = Number(a.split('/')[0])
-                b = Number(b.split('/')[0])
-                if (a > b) {
-                    return 1
-                } else if (a < b) {
-                    return -1
-                } else {
-                    return 0
-                }
-            }
-        }
-    },{
-        name: 'Server Plan',
-        width: '12%',
-        data: row => row[0].staticInfo.serverPlan,
-        sort: {
-            compare: (a, b) => {
-                function getWeight(plan) {
-                    const weight = {
-                        'Free': 0,
-                        'Daily': 1,
-                        'MH20': 2,
-                        'YEARLY MH20': 3,
-                        'MH35': 4,
-                        'YEARLY MH35': 5,
-                        'MH75': 6,
-                        'YEARLY MH75': 7,
-                        'MH Unlimited': 8,
-                        'YEARLY MH Unlimited': 9
-                    }
-                    return weight[plan] || weight['Free']
-                }
-
-                if (getWeight(a) > getWeight(b)) {
-                    return 1;
-                  } else if (getWeight(a) < getWeight(b)) {
-                        return -1;
-                  } else {
-                    return 0;
-                  }
-            }
-        }
-    },{
-        name: 'Connected Servers',
-        data: row => row[0].staticInfo.connectedServers,
-        formatter: (cell) => gridjs.html(cell.join(' ').replace(/([^\s]+)/g, '<a href="./server.html?q=$1" target="_blank">$1</a>') || '-'),
-    },{
-        name: 'Connectable',
-        data: row => row[0].connectable,
-        formatter: (cell) => gridjs.html(cell ? '✔&#xFE0F' : '❌&#xFE0F'),
-        sort: { enabled: false }
-    }],
-    server: {
-        url: 'https://api.minehut.com/servers',
-        then: data => data.servers.map(server => [server])
+    {
+      name: 'Name',
+      data: (row) => row[0].name,
     },
-    style: {
-        td: {
-            'padding': '6px 12px',
-            'color': '#ccc'
+    {
+      name: 'MOTD',
+      data: (row) => row[0].motd,
+      formatter: (cell) => {
+        try {
+          const component = MiniMessage.miniMessage().deserialize(cell)
+          return gridjs.html(MiniMessage.toHTML(component))
+        } catch (error) {
+          return gridjs.html(cell)
         }
+      },
+      sort: { enabled: false },
     },
-    language: {
-        'search': {
-            'placeholder': '🔍 Search...'
+    {
+      name: 'Players',
+      width: '8%',
+      data: (row) => `${row[0].playerData.playerCount}/${row[0].maxPlayers || '?'}`,
+      sort: {
+        compare: (a, b) => {
+          a = Number(a.split('/')[0])
+          b = Number(b.split('/')[0])
+          if (a > b) {
+            return 1
+          } else if (a < b) {
+            return -1
+          } else {
+            return 0
+          }
         },
-        'pagination': {
-            'showing': 'Displaying',
-            'results': () => 'servers'
-        }
-    }
-  }).render(document.getElementById('table'));
+      },
+    },
+    {
+      name: 'Server Plan',
+      width: '12%',
+      data: (row) => row[0].staticInfo.serverPlan,
+      sort: {
+        compare: (a, b) => {
+          function getWeight(plan) {
+            let cleanPlan = plan
+
+            if (plan === 'External Server') {
+              cleanPlan = 'External'
+            }
+
+            if (plan.includes('Custom plan')) {
+              cleanPlan = 'Custom'
+            }
+
+            const weight = {
+              // idk the actual weights so im going to
+              // guess the best sounding ones
+              External: 0,
+              Starter: 1,
+              Standard: 2,
+              Pro: 3,
+              Ultimate: 4,
+              Custom: 5,
+            }
+            return weight[cleanPlan] || 0
+          }
+
+          if (getWeight(a) > getWeight(b)) {
+            return 1
+          }
+
+          if (getWeight(a) < getWeight(b)) {
+            return -1
+          }
+          
+          return 0
+        },
+      },
+    },
+    {
+      name: 'Connected Servers',
+      data: (row) => row[0].staticInfo.connectedServers,
+      formatter: (cell) =>
+        gridjs.html(
+          cell
+            .join(' ')
+            .replace(
+              /([^\s]+)/g,
+              '<a href="./server.html?q=$1" target="_blank">$1</a>'
+            ) || '-'
+        ),
+    },
+    {
+      name: 'Joinable',
+      data: (row) => row[0].connectable,
+      formatter: (cell) => gridjs.html(cell ? '✔&#xFE0F' : '❌&#xFE0F'),
+      sort: { enabled: false },
+    },
+  ],
+  server: {
+    url: 'https://api.minehut.com/servers',
+    then: (data) => {
+      console.log(data)
+
+      return data.servers.map((server) => [server])
+    },
+  },
+  style: {
+    td: {
+      padding: '6px 12px',
+      color: '#ccc',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+    },
+  },
+  language: {
+    search: {
+      placeholder: '🔍 Search...',
+    },
+    pagination: {
+      showing: 'Displaying',
+      results: () => 'servers',
+    },
+  },
+}).render(document.getElementById('table'))
